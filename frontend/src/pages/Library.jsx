937f6d528a05fd4714e6ddfa624d1a576b149939
AbhinavChaudhary11 <abhinavchaudhary1103@gmail.com>
2025-11-15 11:22:27 +0530

    Update Library.jsx

--- frontend/src/pages/Library.jsx
+++ frontend/src/pages/Library.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { IoGrid } from "react-icons/io5";
 import { FaThList } from "react-icons/fa";
 
-
+console.log("API_BASE from Auth:", API_BASE);
 export default function Library() {
   const { token, API_BASE } = useAuth();
   const [library, setLibrary] = useState([]);
